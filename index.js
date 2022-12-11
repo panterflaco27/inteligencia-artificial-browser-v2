3ba47fde94acf2b02d913eefa578f0df1d2c26b1
panterflaco27 <isai_06_12@hotmail.com>
2022-12-10 20:36:24 -0600

    modificar nodos

--- index.js
+++ index.js
@@ -18,40 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 var driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', '123456'));
 var session = driver.session();
 
-
-//muestra pagina de inicio
-app.get('/', function(req, res) {
-    res.render('pages/principal.ejs')
-});
-
-
-
-//Registrar usuario
-/*app.post('/register', function(req, res){
-    var username = req.body.username;
-
-    session
-        .run('CREATE(n:Persona {username:$paramusername}) RETURN n.nombre', {paramusername:username})
-        .then(function(result){
-            session.close();
-            res.status(200).send('USUSARIO REGISTRADO');
-        })
-        .catch(function(err){
-            console.log(err);
-            res.status(500).send('ERROR AL REGISTAR AL USUARIO');
-        });
-})*/
-
-
-
-//Autenticar Usuario
-/*app.post('/authenticate', function(req,res){
-    var buscador = req.body.buscador;
-    
-
-    res.render('pages/principal.ejs');
-})*/
-
+//eliminar relaciones de nodos
 app.post('/eliminarrelacion', function(req, res){
     var nombrea = req.body.nombrea;
     var nombreb = req.body.nombreb;
@@ -67,23 +34,23 @@ app.post('/eliminarrelacion', function(req, res){
         })
 })
 
-
-
+//eliminar nodos
 app.post('/eliminar', function(req, res){
     var nombre = req.body.nombre;
 
     session
         .run('MATCH (a:nodo {nombre: $nombre}) DETACH DELETE a', {nombre:nombre})
         .then(function(result){
-            res.redirect('/views/pages/eliminar.ejs');
+            res.redirect('/views/pages/principal.ejs');
         })
         .catch(function(err){
             console.log(err);
             res.status(200).send('Error al eliminar el nodo!!');
+            res.render('pages/principal.ejs');
         })
 })
 
-
+//crear nodos
 app.post('/create', function(req, res){
     var nombre = req.body.nombre;
     var descripcion = req.body.descripcion;
@@ -98,6 +65,7 @@ app.post('/create', function(req, res){
         });
 })
 
+//crear nuevas relaciones
 app.post('/relacion', function(req, res){
     var nom1 = req.body.nombre1;
     var nom2 = req.body.nombre2;
@@ -109,11 +77,46 @@ app.post('/relacion', function(req, res){
             res.render('pages/principal.ejs');
         })
         .catch(function(err){
-            console.log(err)
+            console.log(err);
             res.status(200).send('Relacion no creada');
         })
 })
 
+
+//modificar nombre de un nodo
+app.post('/modnombre', function(req, res){
+    var viejo = req.body.viejo;
+    var nuevo = req.body.nuevo;
+
+    session
+        .run("MATCH (a:nodo {nombre: $viejo}) SET a.nombre = $nuevo RETURN a", {viejo: viejo, nuevo: nuevo})
+        .then(function(result){
+            res.render('pages/principal.ejs');
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(200).send('Nodo no encontrado!!!');
+        })
+
+})
+
+//modificar descripcion de un nodo especifico
+app.post('/moddesc', function(req, res){
+    var nombre = req.body.nombre;
+    var descripcion = req.body.descripcion;
+
+    session
+        .run('MATCH (a:nodo {nombre: $nombre}) WHERE a.nombre = $nombre SET a.descripcion = $descripcion RETURN a',{nombre: nombre, descripcion: descripcion})
+        .then(function(result){
+            res.render('pages/principal.ejs');
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(200).send('Nodo no encontrado!!!');
+        })
+})
+
+//mostrar los nodos de la base de conocimiento
 app.get('/buscar', function(req,res){
     session
         .run('MATCH(n) RETURN n')
@@ -135,37 +138,38 @@ app.get('/buscar', function(req,res){
         });
 })
 
-app.get('/views/pages/singup.ejs', function(req, res) {
-    res.render('pages/singup.ejs')
+
+
+
+//muestra pagina de inicio
+app.get('/', function(req, res) {
+    res.render('pages/principal.ejs')
 });
 
-app.get('/views/pages/index.ejs', function(req, res) {
-    res.render('pages/index.ejs')
-});
-
+//pagina principal
 app.get('/views/pages/principal.ejs', function(req, res) {
     res.render('pages/principal.ejs')
 });
 
+//pagina de resultados --> tabla con nodos y descripcion
 app.get('/views/pages/result.ejs', function(req, res) {
     res.render('pages/result.ejs')
 });
 
+//pagina apara crear nodos y relaciones
 app.get('/views/pages/create.ejs', function(req, res) {
     res.render('pages/create.ejs')
 });
 
-app.get('/views/pages/alumno.ejs', function(req, res) {
-    res.render('pages/alumno.ejs')
-});
-
-app.get('/views/pages/relacion.ejs',function(req,res){
-    res.render('pages/relacion.ejs')
-});
-
+//pagina para eliminAR nodos y relaciones
 app.get('/views/pages/eliminar.ejs', function(req, res){
     res.render('pages/eliminar.ejs')
-})
+});
+
+//pagina para modificar nodos
+app.get('/views/pages/modificar.ejs',function(req, res){
+    res.render('pages/modificar.ejs')
+});
 
 app.listen(3000);
 console.log('Server Started on Port 3000');
